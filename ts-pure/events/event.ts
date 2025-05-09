cef247ab4c688afc3439739ce6722b8df9653955
Alexander Rudyk <dezo103@gmail.com>
2025-05-09 22:37:37 +0300

    feat: add drag event ex

--- ts-pure/events/event.ts
+++ ts-pure/events/event.ts
@@ -2,4 +2,8 @@
     const myButton: HTMLButtonElement | null = document.querySelector("button");
 
     myButton?.addEventListener("click", (event: MouseEvent) => {})
+
+    const myInput: HTMLInputElement | null  = document.querySelector("input");
+
+    myInput?.addEventListener("dragstart",(event: DragEvent) => {})
 }
